f5dd5bb135c1a9ea15e8ac13bd521de53338095f
Nathan Giddings <nathang567@gmail.com>
2020-08-01 16:13:09 -0500

    Started kernel state class

---
+++ src/kernelstate.hpp
@@ -0,0 +1,21 @@
+#ifndef KERNELSTATE_H
+#define KERNELSTATE_H
+
+#include <stddef.h>
+
+#include "process.hpp"
+
+namespace kernel
+{
+
+class State
+{
+public:
+
+    static const size_t MAX_PROCESSES = 2048;
+
+};
+
+}
+
+#endif
