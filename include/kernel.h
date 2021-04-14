48c042ca5f9d0f4fcf5df38bc5d33324104524ec
ngiddings <nathang567@gmail.com>
2021-04-14 03:39:07 -0500

    Added declaration for panic() function

--- include/kernel.h
+++ include/kernel.h
@@ -14,3 +14,5 @@ extern struct kernel_t kernel;
 int do_syscall(struct kernel_t *kernel, size_t id, size_t arg1, size_t arg2, size_t arg3);
 
 int load_module(struct kernel_t *kernel, struct module_t *module);
+
+void panic(const char *message) __attribute__ ((noreturn));
