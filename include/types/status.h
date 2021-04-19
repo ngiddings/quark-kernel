c5ca5e82a79374efc946856fc8e5921ab0f7f733
ngiddings <nathang567@gmail.com>
2021-04-19 06:56:12 -0500

    Added more status codes

--- include/types/status.h
+++ include/types/status.h
@@ -3,8 +3,11 @@
 enum status_t
 {
     S_OK = 0,
-    S_BAD_SYSCALL,
-    S_OUT_OF_MEMORY,
+    S_NULL_POINTER,
     S_OUT_OF_BOUNDS,
-    S_NULL_POINTER
+    S_INVALID_ARGUMENT,
+    S_OUT_OF_MEMORY,
+    S_DOESNT_EXIST,
+    S_EXISTS,
+    S_BAD_SYSCALL
 };
