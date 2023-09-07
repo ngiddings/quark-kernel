43440310f21bca2d59667ddb5a16bdfe5ecebef3
Nathan Giddings <nathang567@gmail.com>
2023-09-07 00:55:45 -0500

    Added type definitions for process and object IDs

---
+++ include/types/oid.h
@@ -0,0 +1,6 @@
+#ifndef _QUARK_OID_H
+#define _QUARK_OID_H
+
+typedef unsigned int oid_t;
+
+#endif
---
+++ include/types/pid.h
@@ -0,0 +1,6 @@
+#ifndef _QUARK_PID_H
+#define _QUARK_PID_H
+
+typedef unsigned int pid_t;
+
+#endif
